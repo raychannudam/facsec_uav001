437f640f3e98a37b92524e2678f8d2745687aa8d
whatanakKean <85047164+whatanakKean@users.noreply.github.com>
2025-09-27 05:44:32 +0000

    Update JWT refresh and Add Role to UserResponseSchema

--- backend/Schemas/User.py
+++ backend/Schemas/User.py
@@ -1,4 +1,6 @@
 from pydantic import BaseModel, Field
+from typing import List, Optional
+from Schemas.Role import RoleResponseSchema  # import Role schema
 
 class UserCreateSchema(BaseModel):
     email: str = Field(..., description="The email of the user")
@@ -7,13 +9,15 @@ class UserCreateSchema(BaseModel):
     fullname: str = Field(..., description="The full name of the user")
     age: int = Field(..., description="The age of the user")
     gender: str = Field(..., description="The gender of the user")
+    role_ids: Optional[List[int]] = Field([], description="List of role IDs assigned to the user")
 
 class UserUpdateSchema(BaseModel):
-    email: str = Field(..., description="The email of the user")
-    username: str = Field(..., description="The username of the user")
-    fullname: str = Field(..., description="The full name of the user")
-    age: int = Field(..., description="The age of the user")
-    gender: str = Field(..., description="The gender of the user")
+    email: Optional[str] = Field(None, description="The email of the user")
+    username: Optional[str] = Field(None, description="The username of the user")
+    fullname: Optional[str] = Field(None, description="The full name of the user")
+    age: Optional[int] = Field(None, description="The age of the user")
+    gender: Optional[str] = Field(None, description="The gender of the user")
+    role_ids: Optional[List[int]] = Field(None, description="List of role IDs assigned to the user")
 
 class UserResponseSchema(BaseModel):
     id: int = Field(..., description="The ID of the user")
@@ -22,3 +26,4 @@ class UserResponseSchema(BaseModel):
     fullname: str = Field(..., description="The full name of the user")
     age: int = Field(..., description="The age of the user")
     gender: str = Field(..., description="The gender of the user")
+    roles: List[RoleResponseSchema] = Field([], description="List of roles assigned to the user")
