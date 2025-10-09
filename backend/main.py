cd9e34b85c664cd2fce4678ada447d7d46f70658
Channudam Ray <channudam.2002@gmail.com>
2025-10-09 16:56:16 +0700

    Update CORS setting

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],  # You can restrict later to e.g. ["http://localhost:3000"]
+    allow_origins=["http://localhost:8080", "http://localhost:8081"],  # You can restrict later to e.g. ["http://localhost:3000"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
