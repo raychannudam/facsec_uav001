ba7baca063f675d0da8e01b8781942ea6a1c8b9b
Satya2308 <siengsatya0505@gmail.com>
2025-10-11 10:19:17 +0700

    build: Dockerfile and script changes for startup

---
+++ backend/main.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Start Mosquitto in the background
+mosquitto -c mosquitto/config/mosquitto.conf &
+
+# Wait a few seconds to ensure Mosquitto starts
+sleep 2
+
+# Start FastAPI
+uvicorn main:app --host 0.0.0.0 --port 8000 --workers 1 --reload
