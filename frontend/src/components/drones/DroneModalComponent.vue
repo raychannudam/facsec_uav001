66b9031d4dabe8c28c994cce03a2dd6d0ef805cf
Satya2308 <siengsatya0505@gmail.com>
2025-10-18 11:34:55 +0700

    refactor(drones): remove lat/long inputs from form

--- frontend/src/components/drones/DroneModalComponent.vue
+++ frontend/src/components/drones/DroneModalComponent.vue
@@ -78,26 +78,6 @@
                 </div>
 
                 <div class="relative">
-                    <input v-model="formData.last_lat" type="number" id="latitude" step="0.000001"
-                        class="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                        placeholder=" " required />
-                    <label for="latitude"
-                        class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">
-                        Latitude *
-                    </label>
-                </div>
-
-                <div class="relative">
-                    <input v-model="formData.last_long" type="number" id="longitude" step="0.000001"
-                        class="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                        placeholder=" " required />
-                    <label for="longitude"
-                        class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">
-                        Longitude *
-                    </label>
-                </div>
-
-                <div class="relative">
                     <JsonEditorVue v-model="formData.operation_data" mode="text"></JsonEditorVue>
                 </div>
 
@@ -153,8 +133,8 @@ const formData = ref({
     type: '',
     mqtt_client_id: '',
     streaming_client_id: '',
-    last_lat: '',
-    last_long: '',
+    last_lat: 0,
+    last_long: 0,
     operation_data: {}
 })
 
@@ -171,8 +151,8 @@ const resetForm = () => {
         type: '',
         mqtt_client_id: '',
         streaming_client_id: '',
-        last_lat: '',
-        last_long: '',
+        last_lat: 0,
+        last_long: 0,
         operation_data: {}
     }
 }
@@ -184,8 +164,8 @@ watch(() => props.drone, (newDrone) => {
             type: newDrone.type || '',
             mqtt_client_id: newDrone.mqtt_client_id || '',
             streaming_client_id: newDrone.streaming_client_id || '',
-            last_lat: newDrone.last_lat || '',
-            last_long: newDrone.last_long || '',
+            last_lat: 0,
+            last_long: 0,
             operation_data: newDrone.operation_data || {}
         }
     } else {
