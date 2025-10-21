38ad471658d29ec67ac83de880a5e53e9cac6e3c
Channudam Ray <channudam.2002@gmail.com>
2025-10-21 11:42:05 +0700

    Update ControllerStore

--- frontend/src/stores/ControllerStore.js
+++ frontend/src/stores/ControllerStore.js
@@ -30,7 +30,7 @@ export const useControllerStore = defineStore("controller", () => {
         let message = "";
         let data = undefined;
         try {
-            const response = await api.get("/api/v1/controllers");
+            const response = await api.get("/api/v1/controllers/");
             controllers.value = response.data;
             data = response.data;
             if (response.data.length > 0 && !selectedController.value) {
