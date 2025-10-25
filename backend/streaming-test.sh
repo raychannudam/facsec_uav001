f23697908578f86f2d870d24a76375352a446eff
RAY CHANN UDAM <raychannudam@raychannudam.local>
2025-10-25 16:48:35 +0700

    Add streaming-test.sh

---
+++ backend/streaming-test.sh
@@ -0,0 +1,6 @@
+ffmpeg -f avfoundation -framerate 30 -video_size 1280x720 -i "0" \
+  -c:v libx264 -preset veryfast -tune zerolatency \
+  -x264-params "bframes=0:profile=baseline" \
+  -pix_fmt yuv420p \
+  -f rtsp -rtsp_transport tcp \
+  rtsp://test:test@localhost:8554/drsys/test/01
