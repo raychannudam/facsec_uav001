acd48af5f5dc2f0c5de59dbb0de276d2eea40b67
RAY CHANN UDAM <raychannudam@raychannudam.local>
2025-11-09 14:50:20 +0700

    Update MqttTopic Service

--- backend/Services/MqttTopic.py
+++ backend/Services/MqttTopic.py
@@ -67,8 +67,8 @@ class MqttTopicService:
         mqtt_client = db.query(MqttClientModel).filter(MqttClientModel.id == mqtt_client_id).first()
         if mqtt_client_id is not None:
             for topic in db.query(MqttTopicModel).filter(MqttTopicModel.mqtt_client_id == mqtt_client_id).all():
-                if topic.name == f"drsys/{mqtt_client.username}/altitude" or topic.name == f"drsys/{mqtt_client.username}/battery" or topic.name == f"drsys/{mqtt_client.username}/gps_latlng" or topic.name == f"drsys/{mqtt_client.username}/speed":
-                    continue
+                # if topic.name == f"drsys/{mqtt_client.username}/altitude" or topic.name == f"drsys/{mqtt_client.username}/battery" or topic.name == f"drsys/{mqtt_client.username}/gps_latlng" or topic.name == f"drsys/{mqtt_client.username}/speed":
+                #     continue
                 mqtt_topics.append(topic)
         return mqtt_topics
         
