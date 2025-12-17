6a059168e2b5f46ef85884512318bdc8162e5f72
User <user@factoryio.tailea3875.ts.net>
2025-12-17 13:07:10 +0700

    Update Streaming and Backend

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080", "http://localhost:8081", "https://drsys.aitips.digital"],  # You can restrict later to e.g. ["http://localhost:3000"]
+    allow_origins=["http://localhost:8080", "http://localhost:8081", "https://drsys.aitips.digital", "https://dash.drsys.asia"],  # You can restrict later to e.g. ["http://localhost:3000"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
